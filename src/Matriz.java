5b3288bdb01372c1c126e72122851e139b33c1b7
NatallyThiel <145022387+NatallyThiel@users.noreply.github.com>
2025-09-02 19:06:02 -0300

    commit das classes

--- src/Matriz.java
+++ src/Matriz.java
@@ -1,2 +1,11 @@
 public class Matriz {
+    private int[][] dados; //valores da matriz
+    private int linhas;
+    private int colunas;
+
+    public Matriz(int linhas, int colunas) {
+        this.linhas = linhas;
+        this.colunas = colunas;
+        this.dados = new int[linhas][colunas];//criado a mtriz com as dimensões
+    }
 }
